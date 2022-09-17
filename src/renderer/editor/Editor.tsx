7a3a78a55a8be98cec2a0c5018f1562d2d069364
Izzy Lancaster <avrisaac555@gmail.com>
2022-09-17 16:19:36 -0700

    Edtior "works"

--- src/renderer/editor/Editor.tsx
+++ src/renderer/editor/Editor.tsx
@@ -1,4 +1,4 @@
-import { KeyboardEvent, useState } from "react";
+import { FocusEvent, KeyboardEvent, MouseEvent, useState } from "react";
 
 interface EditorBlock {
     text: string;
@@ -13,14 +13,26 @@ export default function Editor() {
         {text: ""}
     ]});
 
-    function handleOnClick(e: KeyboardEvent) {
+    function handleKeyDown(e: KeyboardEvent) {
         e.preventDefault();
+        console.log("Key down");
         setEditorState((prev) => ({
             blocks: [{text: prev.blocks[0]!.text + e.key}]
         }));
     }
 
+    function handleFocus(e: FocusEvent) {
+        console.log("Focused");
+    }
+
+    function handleClick(e: MouseEvent) {
+        console.log("Clicked");
+        e.stopPropagation();
+        e.preventDefault();
+        (e.currentTarget as any).focus();
+    }
+
     return <div id="editor">
-        <p className="editor-block" onKeyDown={handleOnClick}>{editorState.blocks[0]!.text}</p>
+        <p className="editor-block" onClick={handleClick} onFocus={handleFocus} onKeyDown={handleKeyDown} tabIndex={-1}>{editorState.blocks[0]!.text}</p>
     </div>
 }
