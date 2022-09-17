7b28799693dff389bdd7da7fbb860a5b5e6b6229
Izzy Lancaster <avrisaac555@gmail.com>
2022-09-17 11:20:26 -0700

    Added editor component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,8 @@
+import Editor from "./editor/Editor";
+
 export default function App() {
     return <div>
-        <h1>Hello React!</h1>
+        <h1>Editor test</h1>
+        <Editor />
     </div>
 }
---
+++ src/renderer/editor/Editor.tsx
@@ -0,0 +1,4 @@
+export default function Editor() {
+    return <div contentEditable="true">
+    </div>
+}
